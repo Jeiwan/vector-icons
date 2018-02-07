56d79b54858cf09ee3f1a36607153ce0258d853b
Ivan Kuznetsov <me@jeiwan.ru>
2018-02-07 09:16:50 +0700

    Capitalize Ionicons font name

--- Ionicons.js
+++ Ionicons.js
@@ -1,4 +1,4 @@
 import glyphMap from './vendor/react-native-vector-icons/glyphmaps/Ionicons.json';
 import createIconSet from './createIconSet';
 
-export default createIconSet(glyphMap, 'ionicons', require('./fonts/Ionicons.ttf'));
+export default createIconSet(glyphMap, 'Ionicons', require('./fonts/Ionicons.ttf'));
